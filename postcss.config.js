3292fa87e7a6b25654f4655e8f158f420e3da0fb
tommykid13 <128307181+tommykid13@users.noreply.github.com>
2025-08-18 21:22:12 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,8 @@ module.exports = {
     autoprefixer: {},
   },
 };
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: { tailwindcss: {}, autoprefixer: {} },
+};
+export default config;
