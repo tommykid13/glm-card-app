bc568f31a57e1c438cda6153d7ff22bac892b4aa
tommykid13 <128307181+tommykid13@users.noreply.github.com>
2025-08-18 21:23:18 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,3 +10,13 @@ module.exports = {
   },
   plugins: [],
 };
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./lib/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
