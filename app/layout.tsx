d0eaa07c81a8711eb84f19ee3d55cff05de1a869
tommykid13 <128307181+tommykid13@users.noreply.github.com>
2025-08-19 09:06:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { ReactNode } from 'react';
 import './globals.css';
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <head />
-      <link rel="icon" href="/favicon.svg" />
+    <html lang="zh-Hant">
+      <head>
+        <link rel="icon" href="/favicon.svg" />
+      </head>
       <body>{children}</body>
     </html>
   );
